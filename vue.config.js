c5cb02511f9c20ce4fb879245ffdc53b1bd78297
dlohvinov <nsblnr@gmail.com>
2020-02-26 16:41:25 +0200

    fix: api url config changed from demo to dev

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-process.env.VUE_APP_API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'https://demo.webitel.com/api';
+process.env.VUE_APP_API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'https://dev.webitel.com/api';
 // http://192.168.177.199/api
 // http://10.10.10.8:1907
 // http://10.10.10.25:1907
