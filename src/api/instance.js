27edafe1d5b883e6a35b3cb447447f8b35ada232
vasjamel <vasjanja@gmail.com>
2022-03-17 20:12:14 +0200

    refactor: after code review [WTEL-2404]

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,8 +2,6 @@ import { objCamelToSnake, objSnakeToCamel } from "@webitel/ui-sdk/src/scripts/ca
 import eventBus from '@webitel/ui-sdk/src/scripts/eventBus';
 import axios from 'axios';
 
-const isLicenseMissing = (error) => error.response.data.id === 'app.context.authz.license.err';
-
 // global API configuration
 // 'X-Webitel-Access' ~ 'X-Access-Token'
 const instance = axios.create({
@@ -33,18 +31,25 @@ instance.interceptors.request.use(
 );
 
 instance.interceptors.response.use(
+    // show notification abount no license
+    undefined, 
+    (error) => {
+        if (error.response && error.response.data.id === 'app.context.authz.license.err') {
+            eventBus.$emit('notification', { type: 'error', text: 'User has no license grants' });
+        }
+        return Promise.reject(error.response.data);
+    })
+
+instance.interceptors.response.use(
     (response) => {
         return objSnakeToCamel(response.data);
     },
     (error) => { // catches 401 error across all api's
-        if (error.response && error.response.status === 401 && !isLicenseMissing(error)) {
+        if (error.response && error.response.status === 401) {
                 console.warn('intercepted 401');
                 localStorage.removeItem('access-token');
         } else {
           // if error isn't 401, returns it
-          if (isLicenseMissing(error)) {
-            return eventBus.$emit('notification', { type: 'error', text: 'User has no license grants' });
-          }
           eventBus.$emit('notification', { type: 'error', text: error.response.data.detail });
         }
         return Promise.reject(error.response.data);
