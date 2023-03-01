c80481a1279dfdc4fe7c24998907e4a27c7509c4
dlohvinov <nsblnr@gmail.com>
2023-03-01 14:40:13 +0200

    fix: added "silence" config for session check [WTEL-3269]

--- src/api/auth/auth.js
+++ src/api/auth/auth.js
@@ -1,85 +1,94 @@
-import instance from '../instance';
+import instance, { config } from '../instance';
 
 export const login = async (credentials) => {
-    const url = '/login';
+  const url = '/login';
 
-    try {
-        const response = await instance.post(url, credentials);
-        localStorage.setItem('access-token', response.accessToken);
-        postToken();
-    } catch (err) {
-        throw err;
-    }
+  try {
+    const response = await instance.post(url, credentials);
+    localStorage.setItem('access-token', response.accessToken);
+    postToken();
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const register = async (credentials) => {
-    const url = '/signup';
+  const url = '/signup';
 
-    try {
-        await instance.post(url, credentials);
-        await login({ username: credentials.username, password: credentials.password });
-    } catch (err) {
-        throw err;
-    }
+  try {
+    await instance.post(url, credentials);
+    await login({
+      username: credentials.username,
+      password: credentials.password,
+    });
+  } catch (err) {
+    throw err;
+  }
 };
 
 const checkSessionByToken = async () => {
-    const url = '/userinfo';
+  const url = '/userinfo';
 
-    try {
-        await instance.get(url);
-        postToken();
-    } catch (err) {
-        clearToken();
-        throw err;
-    }
+  try {
+    await instance.get(url);
+    postToken();
+  } catch (err) {
+    clearToken();
+    throw err;
+  }
 };
 
 const checkSessionByCookies = async () => {
-    const url = '/login';
+  const url = '/login';
 
-    /* OAUTH CHECK, IF THIS USER IS AUTHENTICATED AND HAS COOKIES */
-    const accessToken = localStorage.getItem('access-token');
-    if (!accessToken) {
-        try {
-            const response = await instance.get(url, { withCredentials: true });
-            localStorage.setItem('access-token', response.accessToken);
-            instance.defaults.headers['X-Webitel-Access'] = localStorage.getItem('access-token') || '';
-        } catch (err) {
-            console.error(err);
-        }
+  /* OAUTH CHECK, IF THIS USER IS AUTHENTICATED AND HAS COOKIES */
+  const accessToken = localStorage.getItem('access-token');
+  if (!accessToken) {
+    try {
+      const response = await instance.get(url, { withCredentials: true });
+      localStorage.setItem('access-token', response.accessToken);
+      instance.defaults.headers['X-Webitel-Access'] = localStorage.getItem('access-token') ||
+        '';
+    } catch (err) {
+      console.error(err);
     }
+  }
 };
 
 const checkCurrentSession = async () => {
+  try {
+    config.silent = true;
     await checkSessionByCookies();
-    return checkSessionByToken();
+    await checkSessionByToken();
+  } catch {} finally {
+    config.silent = false;
+  }
 };
 
 const loadServiceProviders = async ({ domain }) => {
-    const baseUrl = '/login';
-    const url = `${baseUrl}?domain=${domain}`;
-    const response = await instance.get(url);
-    return response;
+  const baseUrl = '/login';
+  const url = `${baseUrl}?domain=${domain}`;
+  const response = await instance.get(url);
+  return response;
 };
 
 const clearToken = () => {
-    localStorage.removeItem('access-token');
-    localStorage.removeItem('domain');
-    instance.defaults.headers['X-Webitel-Access'] = '';
+  localStorage.removeItem('access-token');
+  localStorage.removeItem('domain');
+  instance.defaults.headers['X-Webitel-Access'] = '';
 };
 
 const postToken = () => {
-    const accessToken = localStorage.getItem('access-token');
-    const messageData = { accessToken };
-    parent.postMessage(messageData, '*'); // targetOrigin default: '/'
+  const accessToken = localStorage.getItem('access-token');
+  const messageData = { accessToken };
+  parent.postMessage(messageData, '*'); // targetOrigin default: '/'
 };
 
 const AuthAPI = {
-    login,
-    register,
-    checkCurrentSession,
-    loadServiceProviders,
+  login,
+  register,
+  checkCurrentSession,
+  loadServiceProviders,
 };
 
 export default AuthAPI;
