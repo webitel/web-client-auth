cf0d614f3f84f86d1d82d6220e1ab58c4c0e4f31
dlohvinov <nsblnr@gmail.com>
2023-04-17 17:37:45 +0300

    fix: uncommented token refresh logic [WTEL-3318]

--- src/app.vue
+++ src/app.vue
@@ -13,29 +13,30 @@ import Flicking from '@egjs/vue3-flicking';
 export default {
   name: 'app',
   components: { Flicking },
-  // methods: {
-  //   ...mapActions('auth', {
-  //     checkCurrentSession: 'CHECK_CURRENT_SESSION',
-  //   }),
-  //   setLanguage() {
-  //     const lang = localStorage.getItem('lang');
-  //     if (lang) this.$i18n.locale = lang;
-  //   },
-  //   handlePathQuery() {
-  //     const query = objSnakeToCamel(qs.parse(window.parent.location.search.slice(1))); // query, without "?" sign
-  //     if (query.error || query.errorDescription) this.handleErrorsInQuery(query);
-  //   },
-  //   handleErrorsInQuery({ error, errorDescription }) {
-  //     this.$eventBus.$emit('notification', { type: 'error', text: errorDescription });
-  //   },
-  // },
-  // created() {
-  //   this.checkCurrentSession();
-  //   this.setLanguage();
-  // },
-  // mounted() {
-  //   this.handlePathQuery();
-  // },
+  inject: ['$eventBus'],
+  methods: {
+    ...mapActions('auth', {
+      checkCurrentSession: 'CHECK_CURRENT_SESSION',
+    }),
+    setLanguage() {
+      const lang = localStorage.getItem('lang');
+      if (lang) this.$i18n.locale = lang;
+    },
+    handlePathQuery() {
+      const query = objSnakeToCamel(qs.parse(window.parent.location.search.slice(1))); // query, without "?" sign
+      if (query.error || query.errorDescription) this.handleErrorsInQuery(query);
+    },
+    handleErrorsInQuery({ error, errorDescription }) {
+      this.$eventBus.$emit('notification', { type: 'error', text: errorDescription });
+    },
+  },
+  created() {
+    this.checkCurrentSession();
+    this.setLanguage();
+  },
+  mounted() {
+    this.handlePathQuery();
+  },
 };
 </script>
 
