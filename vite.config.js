61722229f0e050a53e20bac8809aecd085fec3c1
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-02-11 15:35:33 +0200

    chore: rm localhost 8080 port hardcode in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,55 @@
-import vue from '@vitejs/plugin-vue';
-import { defineConfig, loadEnv } from 'vite';
-import { resolve } from 'path';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import vue from "@vitejs/plugin-vue";
+import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+	const env = loadEnv(mode, process.cwd(), "");
 
-  return defineConfig({
-    base: '/app/auth',
-    define: {
-      'process.env': JSON.parse(JSON.stringify(env)
-        .replaceAll('VITE_', 'VUE_APP_')),
-    },
-    server: {
-      port: 8080,
-      host: true,
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern',
-          additionalData: `@use "@/assets/css/main.scss" as *;`,
-        },
-      },
-    },
-    optimizeDeps: {
-      include: ['clipboard-copy', 'deep-equal', 'deepmerge'],
-    },
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src'),
-        '@aliasedDeps/api-services/axios': resolve(__dirname, 'src/api/instance'),
-      },
-    },
-    plugins: [
-      vue(),
-      nodePolyfills({
-        include: ['querystring'],
-      }),
-    ],
-    test: {
-      globals: true,
-      coverage: {
-        enabled: false,
-        reporter: 'json',
-      },
-      environment: 'happy-dom',
-      setupFiles: ['./tests/config/config.js'],
-    },
-  });
-}
+	return defineConfig({
+		base: "/app/auth",
+		define: {
+			"process.env": JSON.parse(
+				JSON.stringify(env).replaceAll("VITE_", "VUE_APP_"),
+			),
+		},
+		server: {
+			// host: true,  // uncomment me to enable localhost access by IP (including from other devices in the network)
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					api: "modern",
+					additionalData: `@use "@/assets/css/main.scss" as *;`,
+				},
+			},
+		},
+		optimizeDeps: {
+			include: ["clipboard-copy", "deep-equal", "deepmerge"],
+		},
+		resolve: {
+			alias: {
+				"@": resolve(__dirname, "src"),
+				"@aliasedDeps/api-services/axios": resolve(
+					__dirname,
+					"src/api/instance",
+				),
+			},
+		},
+		plugins: [
+			vue(),
+			nodePolyfills({
+				include: ["querystring"],
+			}),
+		],
+		test: {
+			globals: true,
+			coverage: {
+				enabled: false,
+				reporter: "json",
+			},
+			environment: "happy-dom",
+			setupFiles: ["./tests/config/config.js"],
+		},
+	});
+};
