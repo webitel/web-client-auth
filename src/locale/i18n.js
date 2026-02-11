ea7156ec0902f614d2bfeb9b0febbf1496faba44
Yehor Hlukhov <egorgluhov68@gmail.com>
2026-02-11 12:28:44 +0200

    fix: remove i18n legacy mode [WTEL-8886](https://webitel.atlassian.net/browse/WTEL-8886)

--- src/locale/i18n.js
+++ src/locale/i18n.js
@@ -1,30 +1,31 @@
-import { createI18n } from 'vue-i18n';
+import { createI18n } from "vue-i18n";
 
-import en from './en/en';
-import es from './es/es';
-import kz from './kz/kz';
-import ro from './ro/ro';
-import ru from './ru/ru';
-import uk from './uk/uk';
-import pl from './pl/pl';
-import uz from './uz/uz';
-import vi from './vi/vi';
+import en from "./en/en";
+import es from "./es/es";
+import kz from "./kz/kz";
+import ro from "./ro/ro";
+import ru from "./ru/ru";
+import uk from "./uk/uk";
+import pl from "./pl/pl";
+import uz from "./uz/uz";
+import vi from "./vi/vi";
 
 const messages = {
-  en,
-  es,
-  ru,
-  ro,
-  uk,
-  kz,
-  pl,
-  uz,
-  vi,
+	en,
+	es,
+	ru,
+	ro,
+	uk,
+	kz,
+	pl,
+	uz,
+	vi,
 };
 
 export default createI18n({
-  allowComposition: true,
-  locale: 'en',
-  fallbackLocale: 'en',
-  messages,
+	legacy: false,
+	allowComposition: true,
+	locale: "en",
+	fallbackLocale: "en",
+	messages,
 });
